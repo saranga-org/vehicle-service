95fda87e52fec062866c37e7364783c83e334e7f
DewramJay <107150266+DewramJay@users.noreply.github.com>
2024-10-20 20:56:09 +0530

    error handle

--- src/main/java/FuelPass/Dev/FuelPass/Controller/VehicleController.java
+++ src/main/java/FuelPass/Dev/FuelPass/Controller/VehicleController.java
@@ -5,6 +5,8 @@ import FuelPass.Dev.FuelPass.DTO.VehicleDTO;
 import FuelPass.Dev.FuelPass.Service.VehicleService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,12 +21,29 @@ public class VehicleController {
 
 
     @PostMapping("/saveVehicle")
-    public VehicleDTO saveVehicle(@RequestBody VehicleDTO vehicleDTO){
-        return vehicleService.saveVehicle(vehicleDTO);
+    public ResponseEntity<VehicleDTO> saveVehicle(@RequestBody VehicleDTO vehicleDTO) {
+        try {
+            VehicleDTO savedVehicle = vehicleService.saveVehicle(vehicleDTO);
+            return ResponseEntity.status(HttpStatus.CREATED).body(savedVehicle);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
     }
 
     @GetMapping("/getVehicles")
-    public List<VehicleDTO> getVehicles(){
-        return vehicleService.getAllVehicles();
+    public ResponseEntity<List<VehicleDTO>> getVehicles() {
+        try {
+            List<VehicleDTO> vehicles = vehicleService.getAllVehicles();
+
+            if (vehicles.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.NO_CONTENT).body(vehicles);
+            }
+
+            return ResponseEntity.status(HttpStatus.OK).body(vehicles);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
     }
 }
